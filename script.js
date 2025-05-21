1aad15c7aeb02bdcfbe8d96028451caade7c32b0
Ihar Skavysh <igogoblin@gmail.com>
2025-05-21 14:19:41 +0300

    feat: add footer

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const thema = document.getElementById("themaToggle");
+const body = document.querySelector("body");
+const thema = document.querySelector(".thema");
+const themaCircle = document.querySelector(".thema__circle");
 
 thema.addEventListener("click", () => {
-  thema.classList.toggle("active");
+  body.classList.toggle("light");
+});
+
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu__btn");
+menuBtn.addEventListener("click", (e) => {
+  menu.classList.add("active");
+  e.stopPropagation();
+});
+
+document.addEventListener("click", (e) => {
+  const isClickInside = menu.contains(e.target);
+
+  if (!isClickInside && !menuBtn.contains(e.target)) {
+    menu.classList.remove("active");
+  }
+  if (e.target.tagName === "A" || e.target.tagName === "LI") {
+    menu.classList.remove("active");
+  }
 });
